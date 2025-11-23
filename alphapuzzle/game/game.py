5dab3d9c4545e3a8d9572e8acda8bf18a2ac88a5
aleezasiddiqui2009 <aleezasiddiqui2009@gmail.com>
2025-11-23 09:16:37 -0800

    Update game.py

--- alphapuzzle/game/game.py
+++ alphapuzzle/game/game.py
@@ -1,16 +1,9 @@
-class Game:
-    def __init__(self, env, agent1, agent2):
-        self.env = env
-        self.agent1 = agent1
-        self.agent2 = agent2
+from alphapuzzle.env.env import AlphaPuzzleEnv
 
-    def play(self):
-        state = self.env.reset()
-        done = False
+def play():
+    env = AlphaPuzzleEnv(size=4)
+    env.reset()
+    env.render()
 
-        while not done:
-            action1 = self.agent1.get_action(state)
-            action2 = self.agent2.get_action(state)
-            state, reward, done = self.env.step(action1, action2)
-
-        return reward
+if __name__ == "__main__":
+    play()
